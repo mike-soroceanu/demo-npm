16f1c403a64bd8af6fb334054b0b2bb9e3139a97
KingMike1998 <mihaisoroceanu6@gmail.com>
2022-09-10 14:10:54 +0300

    Modifiied Dockerfile and added Build config

---
+++ NPM_Build.groovy
@@ -0,0 +1,22 @@
+pipeline {
+    agent {label 'slave01'}
+
+    tools {
+  }
+
+
+    environment {
+		DOCKERHUB_CREDENTIALS=credentials('DockerHub')
+	}
+
+    stages{
+
+
+    stage ('Build container') {
+      steps {
+        sh "docker build . -t mikesoroceanu/simplewebapp-npm-docker:${BUILD_ID}"
+      }
+    }
+	
+  }
+}
