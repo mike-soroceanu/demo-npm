e8f091f561b062aece8202127fbe968dd8bb6617
KingMike1998 <mihaisoroceanu6@gmail.com>
2022-09-10 14:45:12 +0300

    Added login and push to Docker Hub steps

--- NPM_Build.groovy
+++ NPM_Build.groovy
@@ -11,14 +11,33 @@ pipeline {
 	}
 
     stages{
-
- 
-
         stage ('Build container') {
             steps {
                 sh "docker build . -t mikesoroceanu/simplewebapp-npm-docker:${BUILD_ID}"
             }
         }
+
+        stage ('Build container') {
+            steps {
+                sh "docker image tag mikesoroceanu/simplewebapp-npm-docker:${BUILD_ID} mikesoroceanu/simplewebapp-npm-docker:latest"
+            }
+        }
+
+		stage('Login to Docker Hub') {
+
+			steps {
+				sh 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
+			}
+		}
+
+
+		stage('Push image to Docker Hub') {
+
+			steps {
+				sh "docker push mikesoroceanu/simplewebapp-npm-docker:${BUILD_ID}"
+                sh "docker push mikesoroceanu/simplewebapp-npm-docker:latest"
+			}
+		}
 	
   }
 }
