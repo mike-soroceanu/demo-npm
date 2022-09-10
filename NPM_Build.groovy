e2dc1a4d587fec3fa3d848d4d69ca899d8846c1a
KingMike1998 <mihaisoroceanu6@gmail.com>
2022-09-10 14:45:49 +0300

    ...

--- NPM_Build.groovy
+++ NPM_Build.groovy
@@ -17,7 +17,7 @@ pipeline {
             }
         }
 
-        stage ('Build container') {
+        stage ('Add image tag to container') {
             steps {
                 sh "docker image tag mikesoroceanu/simplewebapp-npm-docker:${BUILD_ID} mikesoroceanu/simplewebapp-npm-docker:latest"
             }
